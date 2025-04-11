508d0342e7e1ae0e1909ce79256dda1e1351ccc7
Terence孫 <527028803@qq.com>
2025-04-11 09:20:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ import re
 """
 
 # 阅读次数 默认120次/60分钟
-READ_NUM = int(os.getenv('READ_NUM') or 120)
+# READ_NUM = int(os.getenv('READ_NUM') or 120)
+READ_NUM = int(random.randint(120, 240))
 # 需要推送时可选，可选pushplus、wxpusher、telegram
 PUSH_METHOD = "" or os.getenv('PUSH_METHOD')
 # pushplus推送时需填
